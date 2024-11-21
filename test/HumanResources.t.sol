35b9eb241ba2eb1a0c5643f59e5dc357519eaaf8
Dhruv Jimulia <dj321@ic.ac.uk>
2024-11-21 23:18:43 +0000

    Fix VSCODE Problems

--- test/HumanResources.t.sol
+++ test/HumanResources.t.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.24;
 
-import "forge-std/Test.sol";
-
-// import {Test, console} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {HumanResources} from "../src/HumanResources.sol";
 
 contract HumanResourcesTest is Test {
