11cebc238d7f3c39b9482187202742ca744ebcb0
Dhruv Jimulia <dj321@ic.ac.uk>
2024-11-24 13:24:31 +0000

    Implement oracle and swap logic

--- src/HumanResources.sol
+++ src/HumanResources.sol
@@ -5,8 +5,8 @@ import "./IHumanResources.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../lib/chainlink/interfaces/AggregatorV3Interface.sol";
-import "uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-
+import '../lib/uniswap/interfaces/ISwapRouter.sol';
+import "../lib/weth/IWETH.sol";
 
 contract HumanResources is IHumanResources {
 
@@ -28,8 +28,15 @@ contract HumanResources is IHumanResources {
     address constant CHAINLINK_ETH_USD_FEED = 0x13e3Ee699D1909E989722E753853AE30b17e08c5;
     AggregatorV3Interface constant ETH_USD_FEED = AggregatorV3Interface(CHAINLINK_ETH_USD_FEED);
 
-    address constant UNISWAP_SWAP_ROUTER = 0xE592427A0AEce92De3EbC44e995AE9D83D0A0C62;
+    address constant UNISWAP_SWAP_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
     ISwapRouter constant SWAP_ROUTER = ISwapRouter(UNISWAP_SWAP_ROUTER);
+
+    address constant WETH_ADDRESS = 0x4200000000000000000000000000000000000006;
+    IWETH constant WETH = IWETH(WETH_ADDRESS);
+
+    uint24 constant UNISWAP_FEE = 3000;
+    uint256 constant UNISWAP_DEADLINE = 30;
+    uint256 constant SLIPPAGE = 2;
 
     modifier onlyHRManager {
         require(msg.sender == hrManagerAddress, NotAuthorized());
@@ -79,15 +86,22 @@ contract HumanResources is IHumanResources {
 
     function withdrawSalary() public override onlyEmployee {
         address employee = msg.sender;
-        uint256 amount = convertToCurrentCurrency(employee, computeAccumulatedSalary(employee) + accuredSalaryTillTermination[employee]);
+        uint256 amountInUSD = computeAccumulatedSalary(employee);
+        uint256 amountInUSDC = convertFromUSDToUSDC(amountInUSD);
+        uint256 oracleAmountInETH = convertFromUSDToETH(amountInUSD);
         lastWithdrawn[employee] = block.timestamp;
         accuredSalaryTillTermination[employee] = 0;
+        uint256 amountSent;
         if (isEth[employee]) {
-            // TODO ETH transfer
+            uint256 actualAmountInETH = swapUSDCForWETH(amountInUSDC, oracleAmountInETH * (100 - SLIPPAGE) / 100);
+            WETH.withdraw(actualAmountInETH);
+            amountSent = actualAmountInETH;
+            transferETH(employee, actualAmountInETH);
         } else {
-            USDC.transfer(employee, amount);
+            USDC.transfer(employee, amountInUSDC);
+            amountSent = amountInUSDC;
         }
-        emit SalaryWithdrawn(employee, isEth[employee], amount);
+        emit SalaryWithdrawn(employee, isEth[employee], amountSent);
     }
 
     function switchCurrency() external override onlyActiveEmployee {
@@ -98,7 +112,8 @@ contract HumanResources is IHumanResources {
     }
 
     function salaryAvailable(address employee) external view override returns (uint256) {
-        return convertToCurrentCurrency(employee, computeAccumulatedSalary(employee));
+        uint256 amountInUSD = computeAccumulatedSalary(employee);
+        return isEth[employee] ? convertFromUSDToETH(amountInUSD) : convertFromUSDToUSDC(amountInUSD);
     }
 
     function hrManager() external view override returns (address) {
@@ -115,12 +130,40 @@ contract HumanResources is IHumanResources {
 
     function computeAccumulatedSalary(address employee) private view returns (uint256) {
         uint256 SECONDS_IN_WEEK = 7 * 24 * 3600;
-        uint256 amountInUSD = ((block.timestamp - lastWithdrawn[employee]) * weeklySalary[employee]) / (SECONDS_IN_WEEK);
+        uint256 amountInUSD = (((block.timestamp - lastWithdrawn[employee]) * weeklySalary[employee]) / (SECONDS_IN_WEEK)) + accuredSalaryTillTermination[employee];
         return amountInUSD;
     }
 
-    function convertToCurrentCurrency(address employee, uint256 amountInUSD) private view returns (uint256) {
+    function convertFromUSDToUSDC(uint256 amountInUSD) private pure returns (uint256) {
         uint256 USD_TO_USDC = 1e12;
-        return (isEth[employee]) ? 0 : amountInUSD / USD_TO_USDC;
+        return amountInUSD / USD_TO_USDC;
+    }
+
+    function convertFromUSDToETH(uint256 amountInUSD) private view returns (uint256) {
+        (,int256 ethPrice,,,) = ETH_USD_FEED.latestRoundData();
+        require(ethPrice > 0, "ETH price less than or equal to 0");
+        uint256 ETH_TO_USD = 1e10;
+        return amountInUSD / (uint256(ethPrice) * ETH_TO_USD);
+    }
+
+    function swapUSDCForWETH(uint256 amountInUSDC, uint256 amountOutMinimum) private returns (uint256) {
+        uint256 amountOut = SWAP_ROUTER.exactInputSingle(
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: USDC_ADDRESS,
+                tokenOut: WETH_ADDRESS,
+                fee: UNISWAP_FEE,
+                recipient: address(this),
+                deadline: block.timestamp + UNISWAP_DEADLINE,
+                amountIn: amountInUSDC,
+                amountOutMinimum: amountOutMinimum,
+                sqrtPriceLimitX96: 0
+            })
+        );
+        return amountOut;
+    }
+
+    function transferETH(address recipient, uint256 amount) private {
+        (bool success, ) = recipient.call{value: amount}("");
+        require(success, "Failed to send ETH");
     }
 }
